29f99217ba46ee6bbb63e753dedf2c3446ea5093
CarlosDCN <48950589+CarlosCardona953@users.noreply.github.com>
2019-05-23 11:55:41 -0500

    Final

--- CarlosCardona_Final_16.py
+++ CarlosCardona_Final_16.py
@@ -19,12 +19,12 @@ dv=0.1
 xx=np.random.random()
 yy=np.random.random()
 
-for i in range (10**6):
+#for i in range (10**6):
     
     #La idea es bucar que el tiempo desde una posición aleatoria x y   hasta la ubicación del barco,
     #a una velocidad que se va a ir cambiado,  coincidan con los valores dados inicialmente.
 
-    for j in range (10**6):
+  #  for j in range (10**6):
 
   #Aquí se va variando la velocidad del sonido en pequeños pasos de buscando que pueda solucionar la ecuación y calcular el error
 # si el error es aceptable entonces deja ese valor y lo guarda en un array.
